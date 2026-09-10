9bb2540857a17dd6274257469255306652652654
Mike Ross <mozkarci1991@gmail.com>
2026-09-10 05:17:11 +0300

    fix: testte isNotNull ad cakismasi (drift vs matcher)

--- mobile/test/core/sync/urun_stok_sync_test.dart
+++ mobile/test/core/sync/urun_stok_sync_test.dart
@@ -1,4 +1,3 @@
-import 'package:drift/drift.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:serviscep/core/database/app_database.dart';
 import 'package:serviscep/core/network/sync_api_client.dart';
